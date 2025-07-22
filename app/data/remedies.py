c3300f0f5c64abf571349cc5b5c48888cc0f5aaa
Shyam Kr Baruah <shyamkrbaruahgi@gmail.com>
2025-07-22 22:56:51 +0530

    fixed the mapping

--- app/data/remedies.py
+++ app/data/remedies.py
@@ -9,132 +9,132 @@ remedies = {
         "remedy": "Prevent apple leaf rust by removing nearby juniper hosts, using resistant apple varieties, and applying fungicides like myclobutanil in spring",
         "cause" : "It is caused by the fungus Gymnosporangium juniperi-virginianae, spreading via spores in wet conditions."
     },
-    10: {
+    2: {
         "name": "healthy grape leaf",
         "remedy": "Prevent issues in grape leaves by pruning for airflow, using disease-resistant varieties, and applying fungicides like sulfur to control diseases",
         "cause" : "NA"
     },
-    11: {
+    3: {
         "name": "Grape Carly black rot leaf infection",
         "remedy": "Prevent grape black rot leaf infection by cutting branches for better air flow, cleaning up fallen leaves, and spraying a safe fungicide when the plant starts growing",
         "cause" : "It happens when it is warm and wet, causing dark spots on leaves and rotting fruit."
     },
-    12: {
+    4: {
         "name": "Healthy peach leaf",
         "remedy": "Keep peach leaves healthy by pruning for good airflow, watering evenly, and using natural sprays like neem oil to stop bugs and diseases.",
         "cause" : "NA"
     },
-    13: {
+    5: {
         "name": "Potato early blight",
         "remedy": "Prevent potato early blight by removing old plant parts, spacing plants for air flow, and spraying a safe fungicide when weather is wet",
         "cause" : "It happens in warm, damp conditions, causing dark spots with rings on leaves that can spread to tubers."
     },
-    14: {
+    6: {
         "name": "Potato late blight",
         "remedy": "Prevent potato late blight by removing old plant debris, spacing plants for good airflow, and using a fungicide during wet, cool weather.",
         "cause" : "It happens in damp conditions, causing dark, watery spots on leaves and stems that can ruin the whole crop."
     },
-    15: {
+    7: {
         "name": "Healthy raspberry leaf",
         "remedy": "Keep raspberry leaves healthy by pruning for good airflow, watering evenly, and using natural sprays like neem oil to stop pests and diseases",
        "cause" : "NA"
     },
-    16: {
+    8: {
         "name": "Healthy soybean",
         "remedy": "Keep soybean leaves healthy by spacing plants for good airflow, watering evenly, and using natural sprays like neem oil to stop pests and diseases.",
         "cause" : "NA"
     },
-    17: {
+    9: {
         "name": "Squash powdery mildew",
         "remedy": "Prevent squash powdery mildew by spacing plants for good airflow, watering at the base, and using a milk spray or sulfur dust when you see white spots",
         "cause" : "It happens in warmm dry weather, causing a white , powdery coating on leaves taht can hurt growth"
     },
-    18: {
+    10: {
         "name": "Healthy strawberry",
         "remedy": "Ensure good air circulation and avoid excess moisture.Use clean tools and remove infected plants promptly.Apply organic fungicides like neem oil early.Remove and destroy affected parts to stop spread.",
         "cause" : "NA"
     },
-    19: {
+    11: {
         "name": "Tomato early blight",
         "remedy": "Rotate crops and avoid planting tomatoes in the same spot every year.Water at the base to keep leaves dry and reduce fungal growth.Apply copper-based fungicides or neem oil at early stages.Remove infected leaves and improve air circulation around plants.",
          "cause" : "Fungal spores in soil or plant debris spread by wind or water.Warm, humid conditions accelerate the disease development."
     },
-    2: {
+    12: {
         "name": "apple Venturia inaequalis",
         "remedy": "Prevent Venturia inaequalis (apple scab) by using resistant apple varieties, pruning for airflow, and applying fungicides like sulfur in early spring.",
-         "cause" : ""
+         "cause" : "It is caused by a fungus spreading in wet, cool conditions, leading to dark, velvety leaf spots."
     },
-    20: {
+    13: {
         "name": "Tomato early blight",
         "remedy": "dfdf",
          "cause" : "Fungal spores in soil or plant debris spread by wind or water. Warm, humid conditions accelerate the disease development."
     },
-    21: {
+    14: {
         "name": "Tomato bacterial spot and speck",
         "remedy": "Use certified disease-free seeds and resistant varieties.Avoid overhead watering and ensure proper spacing.Apply copper-basedbactericides during early symptoms.Remove and destroy infected leaves to control spread",
          "cause" : "Caused by Xanthomonas (spot) and Pseudomonas syringae (speck).Spread through contaminated tools, water, or wind-splashed soil."
     },
-    22: {
+    15: {
         "name": "Tomato late blight",
         "remedy": "Plant resistant varieties and avoid overhead irrigation.Remove weeds and debris that harbor the pathogen.Apply fungicides like chlorothalonil or mancozeb.Destroy infected plants immediately to limit infection.",
          "cause" : "Caused by Phytophthora infestans, a water mold.Spreads rapidly in cool, wet conditions."
     },
-    23: {
+    16: {
         "name": "Tomato mosaic virus",
         "remedy": "Disinfect tools and wash hands after handling infected plants.Use virus-free seeds and resistant varieties.No cure; remove and destroy infected plants.Practice crop rotation and sanitation to prevent spread.",
         "cause" : "Caused by Tomato mosaic virus.Spread through mechanical contact, contaminated tools, or hands."
     },
-    24: {
+    17: {
         "name": "Tomato yellow leaf curl virus",
         "remedy": "Use resistant varieties and control whitefly vectors.Install yellow sticky traps and insect nets.No direct cure; remove infected plants early.Control whiteflies with neem oil or insecticides.",
         "cause" : "Spread by whiteflies carrying the TYLCV virus.Thrives in warm, dry climates with dense plantings."
     },
-    25: {
+    18: {
         "name": "Tomato leaf mold",
         "remedy": "Ensure good air circulation and avoid leaf wetness.Use resistant varieties in greenhouse settings.Apply sulfur or copper-based fungicides.Remove infected leaves and reduce humidity.",
         "cause" : "Caused by the fungus Fulvia fulva.Common in humid environments with poor ventilation."
     },
-    26: {
+    19: {
         "name": "Tomato septoria leaf blight  ",
         "remedy": "Avoid overhead watering and plant in sunny areas.Practice crop rotation and space plants properly.Spray with fungicides like mancozeb or chlorothalonil.Remove and destroy infected lower leaves promptly.",
         "cause" : "Caused by the fungus Septoria lycopersici.Spreads via rain splash from infected plant ."
     },
-    27: {
+    20: {
         "name": "Tomato leaf mold",
         "remedy": "Ensure good air circulation and avoid leaf wetness.Use resistant varieties in greenhouse settings.Apply sulfur or copper-based fungicides.Remove infected leaves and reduce humidity.",
         "cause" : "Caused by the fungus Fulvia fulva.Common in humid environments with poor ventilation."
     },
-    3: {
+    21: {
         "name": "Healthy pepper leaf",
         "remedy": "Prevent issues in pepper leaves by watering consistently, using well-drained soil, and applying neem oil or fungicides to control pests and fungi",
         "cause" : "NA"
     },
-    4: {
+    22: {
         "name": "pepper leaf bacteria spot",
         "remedy": "Prevent bacterial leaf spot on peppers by using resistant varieties, avoiding overhead watering, and applying copper-based bactericides early. ",
         "cause" : "It is caused by Xanthomonas campestris pv. vesicatoria, spreading in warm, wet conditions, causing dark, water-soaked leaf spots"
     },
-    5: {
+    23: {
         "name": "Blueberry leaf disease",
         "remedy": "Prevent blueberry leaf diseases by pruning for airflow, using resistant varieties, and applying fungicides like captan for issues like leaf spot. ",
         "cause" : "Common diseases like Gloeocercospora leaf spot or powdery mildew are caused by fungi, thriving in humid conditions, leading to spots or powdery coatings."
     },
-    6: {
+    24: {
         "name": "Healthy cherry leaf",
         "remedy": "Prevent cherry leaf issues by pruning for airflow, using well-drained soil, and applying fungicides or neem oil to control pests and diseases.",
         "cause" : "NA"
     },
-    7: {
+    25: {
         "name": "corn gray leaf spot",
         "remedy": "Prevent corn gray leaf spot by using resistant hybrids, rotating crops, and applying fungicides like azoxystrobin during wet periods.",
         "cause" : "It is caused by the fungus Cercospora zeae-maydis, thriving in humid conditions, leading to grayish-brown, rectangular leaf lesions."
     },
-    8: {
+    26: {
         "name": "Corn blight",
         "remedy": "Prevent corn blight by using resistant varieties, rotating crops, and applying fungicides like mancozeb during early growth",
         "cause" : "It is caused by fungi like Exserohilum turcicum (northern corn leaf blight), leading to long, gray-green lesions on leaves in humid conditions."
     },
-    9: {
+    27: {
         "name": "Corn common rust",
         "remedy": "Prevent corn common rust by using resistant hybrids, ensuring good air circulation, and applying fungicides like triazoles during wet weather.",
         "cause" : "It is caused by the fungus Puccinia sorghi, leading to reddish-brown pustules on leaves in warm, humid conditions."
